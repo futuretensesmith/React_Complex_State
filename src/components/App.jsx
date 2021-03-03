c2590c42258769486d1a8be3d111d08733ae5a74
Kevin Smith <futuretense@gmail.com>
2021-03-02 18:28:53 -0800

    Redundant code but works

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [first, updateFirst] = useState("");
+  const [last, updateLast] = useState("");
+
+  function firstName(event) {
+    updateFirst(event.target.value);
+    //console.log(event.target.value)
+  }
+  function lastName(event) {
+    updateLast(event.target.value);
+    //console.log(event.target.value)
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {first} {last}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input
+          onChange={firstName}
+          name="fName"
+          placeholder="First Name"
+          value={first}
+        />
+        <input
+          onChange={lastName}
+          name="lName"
+          placeholder="Last Name"
+          value={last}
+        />
         <button>Submit</button>
       </form>
     </div>
